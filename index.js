5fb8fd06ada7fcd0af51b5294dab78c532057a91
pisonpeakgroup <pisonpeakgroup@gmail.com>
2017-03-27 10:09:17 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+# week-2
+
+Write a function that would display the current day and time to the console
+<!--
+var CurrentDay = new Function("Sunday", "Monday","Tuesday","Wednesday",Thursday","Friday","Saturday"; "return Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday;");
+CurrentDay secondFunction(){
+var result;
+result = func(1,2,3,4,5,6,7);
+document.write ( result );
+}
+{
+var Time = new Function("1", "2", "3", ........"12", "return 1,2,3, ........12;");
+function secondFunction(){
+var result;
+result = func(1,2,3,.......12);
+document.write ( result );
+}
+
+
+//-->
+
