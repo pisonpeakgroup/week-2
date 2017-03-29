19ce4d875465c02bbdde6069a04e0eaf1b50da84
pisonpeakgroup <pisonpeakgroup@gmail.com>
2017-03-29 06:19:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,70 @@ document.write ( result );
 
 //-->
 
+
+
+<script type="text/javascript">
+<!--
+var variablename = new Function(Arg1, Arg2..., "Function Body");
+//-->
+</script>
+
+
+
+EXAMPLE
+<script type="text/javascript">
+<!--
+var func = new Function("x", "y", "return x*y;");
+function secondFunction(){
+var result;
+result = func(10,20);
+document.write ( result );
+}
+//-->
+</script>
+
+
+Write a function that would display the current day and time to the console
+<!--
+var CurrentDay = new Function("Sunday", "Monday","Tuesday","Wednesday",Thursday","Friday","Saturday"; "return Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday;");
+CurrentDay secondFunction(){
+var result;
+result = func(1,2,3,4,5,6,7);
+document.write ( result );
+}
+{
+var Time = new Function("1", "2", "3", ........"12", "return 1,2,3, ........12;");
+function secondFunction(){
+var result;
+result = func(1,2,3,.......12);
+document.write ( result );
+}
+
+<title>function to reverse a string<title/>
+ -<script type="text/javascript">
+<!--
+ -var str1 = new String( "This is a string" );
+ -var index = str1.localeCompare( "XYZ" );
+ -document.write("localeCompare first :" + index );
+ -document.write("<br />" );
+ -var index = str1.localeCompare( "AbCD ?" );
+ -document.write("localeCompare second :" + index );
+ -}
+ -//-->
+
+
+//-->
+
+function to find the area and perimeter of a triangle. The function would accept the length of its three sides as parameters
+ 
++         <script type="text/javascript">    
+ +            function hypotenuse(a, b, c) {
+ +                function triangle(x) { return x*x*x; }
+ +                return Math.trig(triangle(a) + triangle(b) + triangle(C));
+ +            }
+ +
+ +            function secondFunction() {
+ +                var result;
+ +                result = hypotenuse(1, 2);
+ +                document.write ( result );
+ +            }
