e0aab777a043a662eaf60dbdaffb802d8ee39d20
pisonpeakgroup <pisonpeakgroup@gmail.com>
2017-04-11 14:59:31 +0100

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,56 @@
+var today = CurrentDate('April 12, 2017');    // to show date on the console
+vReturn = Date();
+{
+          return Result;
+}
+
+
+
+Date.prototype.getDay("Wednesday",Thursday","Friday","Saturday","Sunday","Monday","Tuesday"; "return string respectively;");    // to show current day on the console
+{
+var result;
+result = func();
+document.write (result);
+}
+
+
+
+Date.prototype.getTime( 03:24:00);  // to show curent time to the console
+{
+var result;
+result = func();
+document.write ( result );
+}
+
+
+
+function hypotenuse(a, b, c) {
+                 function triangle(2) { return 2*2*2; }
+                 return Math.trig(triangle(a) + triangle(b) + triangle(C));
+             }
+ 
+             function secondFunction() {
+                 var result;
+                 result = hypotenuse(1, 2,3);
+                 document.write ( result );
+             }
+
+
+
+
+console.log("This is my own string");   // a string
+{
+var result;
+result = func(NaN);
+document.write ( result );
+}
+
+
+
+
+var func = new Function("My fisrt string", "my second string", "return My first string*my second string;");
+function secondFunction(){
+var result;
+result = func(NaN);
+document.write ( result );
+}
