567dc2b642aad0bd787956006364ecb458ac0a1c
pisonpeakgroup <pisonpeakgroup@gmail.com>
2017-04-21 13:50:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,19 @@
-# week-2
+// Mentorship String
+var mentorship = new String();
+console.log("Some mentees can be very slow at learning sometimes");
+console.log("Mr Winner is really Amazing");
+console.log("This Mentorship Program has been awsome");
 
-Write a function that would display the current day and time to the console
-<!--
-var CurrentDay = new Function("Sunday", "Monday","Tuesday","Wednesday",Thursday","Friday","Saturday"; "return Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday;");
-CurrentDay secondFunction(){
-var result;
-result = func(1,2,3,4,5,6,7);
-document.write ( result );
-}
-{
-var Time = new Function("1", "2", "3", ........"12", "return 1,2,3, ........12;");
-function secondFunction(){
-var result;
-result = func(1,2,3,.......12);
-document.write ( result );
-}
+//date object
+var date = Date()
+console.log("This is the current time");
+new Date();
 
 
-//-->
+//Calculated parameters of a triangle
+var triangle = new Object();
+triangle.side1 = 3;
+triangle.sede2 = 25;
+triangle.side3 = 25;
+triangle.getside = Math.hypot(30, 34, 34);
 
-
-
-<script type="text/javascript">
-<!--
-var variablename = new Function(Arg1, Arg2..., "Function Body");
-//-->
-</script>
-
-
-
-EXAMPLE
-<script type="text/javascript">
-<!--
-var func = new Function("x", "y", "return x*y;");
-function secondFunction(){
-var result;
-result = func(10,20);
-document.write ( result );
-}
-//-->
-</script>
-
-
-Write a function that would display the current day and time to the console
-<!--
-var CurrentDay = new Function("Sunday", "Monday","Tuesday","Wednesday",Thursday","Friday","Saturday"; "return Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday;");
-CurrentDay secondFunction(){
-var result;
-result = func(1,2,3,4,5,6,7);
-document.write ( result );
-}
-{
-var Time = new Function("1", "2", "3", ........"12", "return 1,2,3, ........12;");
-function secondFunction(){
-var result;
-result = func(1,2,3,.......12);
-document.write ( result );
-}
-
-<title>function to reverse a string<title/>
- -<script type="text/javascript">
-<!--
- -var str1 = new String( "This is a string" );
- -var index = str1.localeCompare( "XYZ" );
- -document.write("localeCompare first :" + index );
- -document.write("<br />" );
- -var index = str1.localeCompare( "AbCD ?" );
- -document.write("localeCompare second :" + index );
- -}
- -//-->
-
-
-//-->
-
-function to find the area and perimeter of a triangle. The function would accept the length of its three sides as parameters
- 
-+         <script type="text/javascript">    
- +            function hypotenuse(a, b, c) {
- +                function triangle(x) { return x*x*x; }
- +                return Math.trig(triangle(a) + triangle(b) + triangle(C));
- +            }
- +
- +            function secondFunction() {
- +                var result;
- +                result = hypotenuse(1, 2);
- +                document.write ( result );
- +            }
